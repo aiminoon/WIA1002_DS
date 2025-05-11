7b3747de854dabc17e21de162b2d2c9c5f128c10
Dennis Aimin Oon bin Jeffrey Oon <149665483+aiminoon@users.noreply.github.com>
2025-05-11 17:11:27 +0800

    Create T7aQ1.java

---
+++ Tutorials/Tutorial7a/T7aQ1.java
@@ -0,0 +1,16 @@
+package T7a;
+
+public class T7aQ1 {
+	public static void main(String[] args) {
+		/* 
+		 * a) Packet switching
+		 * b) Escalator
+		 * c) Car wash
+		 * d) Playground, i.e, Legoland
+		 * e) Music playlist
+		 * f) Restaurant order
+		 * g) Vehicle on one way road
+		 */
+	 	
+	}
+}
