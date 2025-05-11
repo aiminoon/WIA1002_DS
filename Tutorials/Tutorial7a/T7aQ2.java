9ccb6f24c82c79637809006a9c3345d6f2b0264e
Dennis Aimin Oon bin Jeffrey Oon <149665483+aiminoon@users.noreply.github.com>
2025-05-11 17:11:46 +0800

    Create T7aQ2.java

---
+++ Tutorials/Tutorial7a/T7aQ2.java
@@ -0,0 +1,15 @@
+package T7a;
+
+public class T7aQ2 {
+
+	/*
+	 * Queue 
+	 * - Follows FIFO (First-in-First-out) 
+	 * - The first item removed is the item least recently added
+	 * 
+	 * Stack 
+	 * - Follows LIFO (Last-in-First-out) 
+	 * - The first item removed is the item most recently added
+	 */
+
+}
