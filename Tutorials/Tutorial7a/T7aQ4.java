29202f35df777a27443fd4b0618a18efb6ff5586
Dennis Aimin Oon bin Jeffrey Oon <149665483+aiminoon@users.noreply.github.com>
2025-05-11 17:12:49 +0800

    Create T7aQ4.java

---
+++ Tutorials/Tutorial7a/T7aQ4.java
@@ -0,0 +1,35 @@
+package T7a;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class T7aQ4 {
+	public static void main(String[] args) {
+		java.util.Queue<Integer> X = new LinkedList<>();
+		X.add(14);
+		X.add(3);
+		X.add(5);
+		Object Y = X.poll();
+		X.add(7);
+		X.add(9);
+		Y = X.poll();
+		X.add(2);
+		X.add(4);
+		
+		// a) X.front() = 5
+		System.out.println("a) X.peek() = " + X.peek());
+		
+		// b) Y = 5, X.front() = 7
+		Y = X.poll();
+		X.add(10);
+		System.out.println("b) Y = " + Y);
+		System.out.println("b) X.peek() = " + X.peek());
+		
+		// c) Y = 7
+		Y = X.poll();
+		System.out.println("c) Y = " + Y);
+		
+		// d) X.front() = 9
+		System.out.println("d) X.peek() = " + X.peek());
+	}
+}
