9ffccfba2146d6a84d56eb5aadda0ba5eae6d42d
Dennis Aimin Oon bin Jeffrey Oon <149665483+aiminoon@users.noreply.github.com>
2025-05-12 23:16:28 +0800

    Create StorePairGeneric.java

---
+++ Labs/Lab2/L2Q1/StorePairGeneric.java
@@ -0,0 +1,42 @@
+package Lab2.L2Q1;
+
+public class StorePairGeneric<T extends Comparable<T>> implements Comparable<StorePairGeneric<T>> {
+	private T first, second;
+	
+	public StorePairGeneric(T first, T second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+	public T getFirst() {
+		return this.first;
+	}
+	
+	public T getSecond() {
+		return this.second;
+	}
+	
+	public void setPair(T first, T second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+	@Override
+	public String toString() {
+		return "first = " + first + " second = " + second;
+	}
+	
+	@Override
+    public int compareTo(StorePairGeneric<T> another) {
+        return this.first.compareTo(another.first);
+    }
+	
+	@Override @SuppressWarnings("unchecked")
+	public boolean equals(Object anotherObject) {
+		if(this == anotherObject) return true;
+		if(anotherObject == null || anotherObject.getClass() != this.getClass()) return false;	    
+		StorePairGeneric<T> another = (StorePairGeneric<T>) anotherObject;
+	    return another.getFirst().equals(this.getFirst());
+	}
+	
+}
