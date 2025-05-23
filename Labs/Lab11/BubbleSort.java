de54ce1b8a74981bc95b0e1fe78dd41588e59ee0
Dennis Aimin Oon bin Jeffrey Oon <149665483+aiminoon@users.noreply.github.com>
2025-05-23 22:45:50 +0800

    Create BubbleSort.java

---
+++ Labs/Lab11/BubbleSort.java
@@ -0,0 +1,28 @@
+package Lab11;
+
+public class BubbleSort extends SortingAlgorithm {
+	public static final String NAME = "Bubble Sort";
+	
+	BubbleSort() {
+		super(NAME);
+	}
+	
+	public <T extends Comparable<T>> void sort(T[] arr, boolean isAscending) {
+		int flag = (isAscending) ? 1 : -1;
+		boolean isSwap = false;
+		
+		for(int i = 0; i < arr.length; i++) {
+			isSwap = false;
+			for(int j = 0; j < arr.length - 1; j++) {
+				if(flag * arr[j].compareTo(arr[j + 1]) > 0) {
+					T tmp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = tmp;
+					isSwap = true;
+				}
+			}
+			
+			if(!isSwap) break; 
+		}
+	}
+}
